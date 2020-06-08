bd30aaa7442632755e48f2b505dc6e922a1a0dbe
hsok0070 <rlaalsdn777@hanmir.com>
2020-06-08 19:44:24 +0900

    [ADD] (props 공부) Home.vue(부모컴포넌트)에서 MinWoo.vue(자식컴포넌트)로 데이터 보내기 자식 컴포넌트에서 props를 이용하여 부모컴포넌트에서 보낸 데이터를 받을 수 있다. 하지만 자식 컴포넌트에서 받은 데이터를 직접적으로 변경 하는것은 warn을 발생 시킨다.

---
+++ vue-props/src/components/MinWoo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>{{ title }}</h1>
+        <p> {{ name }}</p><br>
+        <button @click='updateName'>Change Name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            /* name: 'MinWoo', */ //props에서 name을 사용중이기 때문에 다른 곳에서는 사용 불가능 
+        }
+    },
+    methods: {
+        updateName() {
+            this.name ='MinWoo updated';
+        }
+    },
+    props: { // props로 받은 데이터를 직접적으로 변경 불가능 warn 발생 부모에서 변경하는 방법을 사용해야한다.
+        title: {
+            type: String,
+            required: true,
+            default: 'default title', 
+        },
+        name: {
+            type: String,
+            required: true,
+            default: 'default name', 
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-props/src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+    <h1>This is Home page</h1>
+    <MinWoo title='home title' :name='name'></MinWoo>
+  </div>
+</template>
+
+<script>
+import MinWoo from '@/components/MinWoo.vue'
+
+export default {
+  data () {
+    return {
+      name:'MinWoo',
+    }
+  },
+  components: {
+    MinWoo,
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
