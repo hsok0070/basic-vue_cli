99d79f5ec117efbbe2fa2db2b8095ec90de8844e
hsok0070 <rlaalsdn777@hanmir.com>
2020-06-08 19:10:19 +0900

    [ADD] (싱글 파일 컴포넌트 공부) 기존 Home.vue 내용을 간단하게 변경후 새로운 components폴더에 MinWoo.vue라는 컴포넌트를 생성 후 Home.vue 와 About.vue에 MinWoo.vue 컴포넌트를 추가 하여 화면에 정상적으로 나타나는지 확인.

---
+++ vue-single_file_component/src/components/MinWoo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <p> {{ name }}</p><br>
+        <button @click='updateName'>Change Name</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            name: 'MinWoo',
+        }
+    },
+    methods: {
+        updateName() {
+            this.name ='MinWoo updated';
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue-single_file_component/src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <MinWoo></MinWoo>
+  </div>
+</template>
+
+<script>
+import MinWoo from '@/components/MinWoo.vue'
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    MinWoo,
+  }
+}
+</script>
