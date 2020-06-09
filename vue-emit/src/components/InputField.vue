620a25245225cecde96e382bf8e30679a5067048
hsok0070 <rlaalsdn777@hanmir.com>
2020-06-09 18:38:44 +0900

    [ADD] (부모 컴포넌트로 데이터 보내기 Emit) InputField.vue(자식컴포넌트)에서 $emit을 통하여 Home.vue(부모컴포넌트)로 데이터를 전달하여 받은 데이터를 변경 하는 메소드를 만들어 변경을 한다.

---
+++ vue-emit/src/components/InputField.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <label for="">Name : </label>
+        <input 
+        type="text" 
+        :value="value"
+        style="padding: 5px; border: 2px solid green;"
+        @input="$emit('input', $event.target.value)" 
+        ><br>
+        <!-- 기존 @input="updateName"를 간단하게 사용 하는방법 updateName 메소드를 만들지 않고 @input에 바로 $emit 가능 -->
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+
+        }
+    },
+    /* props: {
+        name: {
+            type: String,
+            require: true,
+            default: 'defualt name',
+        }
+    }, */
+    props: { // 부모컴포넌트에서 v-model을 사용하여 보낼때
+        value: {
+            type: String,
+            require: true,
+            default: 'defualt name',
+        }
+    },
+    /* methods: {
+        updateName(e) {
+            console.log(e.target.value);
+            this.$emit('update-name', e.target.value)
+        }
+    } */
+}
+</script>
---
+++ vue-emit/src/views/Home.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h1>This is Home page</h1>
+    <form action="">
+      <!-- <InputField :name='name' @update-name='name = $event.target.value'></InputField> -->
+      <!-- 기존 @update-name='updateNmae'를 간단하게 작성하는 방법 updateName 메소드를 만들지 않고 자식컴포넌트에서 보낸 이벤트를
+      InputField 안에서 $event 를 사용하기 -->
+      <InputField v-model="name"></InputField> <!-- 컴포넌트에서 v-model 사용 가능! 자식컴포넌트에 value값을 보낸다.-->
+      <br><button>Submit</button> 
+      <br>
+    </form>
+    {{ name }}
+  </div>
+</template>
+
+<script>
+import InputField from '@/components/InputField.vue'
+
+export default {
+  data () {
+    return {
+      name:'',
+    }
+  },
+  components: {
+    InputField,
+  },
+  /* methods: {
+    updateName(name) {
+      this.name = name;
+    }
+  } */
+}
+</script>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
