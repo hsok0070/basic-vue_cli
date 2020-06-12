f2c7def2cec0e1a83274e66fd651d8d6be61cb01
hsok0070 <rlaalsdn777@hanmir.com>
2020-06-12 21:48:33 +0900

    [ADD] (진행중) todo list 만들어보기

---
+++ todo/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app" class="container">
+    <h1 class='text-center'>Todo app</h1>
+    <input
+    v-model='todoText' 
+    type="text" 
+    class='w-100 p-2' 
+    placeholder="Type todo"
+    @keyup.enter="addTodo"
+    >
+    <hr>
+    <Todo 
+    v-for="todo in todos" 
+    :key="todo.key"
+    :todo="todo"></Todo>
+  </div>
+</template>
+
+<script>
+import Todo from '@/components/Todo.vue';
+
+export default {
+  data() {
+    return {
+      todoText:'',
+      todos: [
+        { id: 1, text: 'buy a car', checked: false}, 
+        { id: 2, text: 'play game', checked: false}, 
+      ]
+    }
+  },
+  components: {
+    Todo
+  },
+  methods: {
+    addTodo(e) {
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false,
+        });
+        this.todoText = '';
+    }
+  }
+}
+</script>
---
+++ todo/src/components/Todo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+      {{ todo.checked}}
+      <input type="checkbox" :checked="todo.checked">
+      <span class="ml-3"> {{ todo.text }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    props: {
+        todo: {
+            type: Object,
+            required: true,
+            default: 'default text',
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
